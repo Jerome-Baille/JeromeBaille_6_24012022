5d6007fb324652e60829fc1d98126fc0982fb0fa
Jerome-Baille <baille.jerome@gmail.com>
2022-02-01 00:44:37 +0100

    Ajout de la fonction like

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -50,5 +50,28 @@ exports.deleteSauce = (req, res, next) =>{
 }
 
 
-
-
+exports.likeSauces = (req, res, next) => {
+    if (req.body.like === 1) { 
+      Sauce.updateOne({ _id: req.params.id }, { $inc: { likes: req.body.like++ }, $push: { usersLiked: req.body.userId } }) 
+        .then(() => res.status(200).json({ message: `J'aime cette sauce !` }))
+        .catch(error => res.status(400).json({ error }))
+    } else if (req.body.like === -1) {
+      Sauce.updateOne({ _id: req.params.id }, { $inc: { dislikes: (req.body.like++) * -1 }, $push: { usersDisliked: req.body.userId } })
+        .then(() => res.status(200).json({ message: `Je n'aime pas cette sauce !` }))
+        .catch(error => res.status(400).json({ error }))
+    } else {
+      Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+          if (sauce.usersLiked.includes(req.body.userId)) {
+            Sauce.updateOne({ _id: req.params.id }, { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } })
+              .then(() => res.status(200).json({ message: `J'annule mon vote` }))
+              .catch(error => res.status(400).json({ error }))
+          } else if (sauce.usersDisliked.includes(req.body.userId)) {
+            Sauce.updateOne({ _id: req.params.id }, { $pull: { usersDisliked: req.body.userId }, $inc: { dislikes: -1 } })
+              .then(() => res.status(200).json({ message: `J'annule mon vote` }))
+              .catch(error => res.status(400).json({ error }))
+          }
+        })
+        .catch(error => res.status(400).json({ error }))
+    }
+}
