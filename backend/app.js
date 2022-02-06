fbbd2a377d5a8d87e3cec61b861cb2351bf7e9f4
Jerome-Baille <baille.jerome@gmail.com>
2022-02-06 23:06:03 +0100

    Ajout de protocoles de sécurités

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const helmet = require("helmet");
 const mongoose = require('mongoose');
 const path = require('path');
 
-require ("dotenv").config();
+const helmet = require("helmet"); // A collection of 13 middleware functions for setting HTTP response headers
+const mongoSanitize = require('express-mongo-sanitize'); // Sanitize user input to protect the system from a MongoDB operator injection
+const rateLimit = require ('express-rate-limit'); // Protect the system against brute force
+require ("dotenv").config(); // Loads environment variables from .env file
 
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
 
-mongoose.connect(`mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@cluster0.ijb56.mongodb.net/Test?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_SERVER}?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
-app.use(helmet());
+
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -26,9 +28,28 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+app.use(helmet());
+
+// By default, $ and . characters are removed completely from user-supplied input in the following places:
+// - req.body
+// - req.params
+// - req.headers
+// - req.query
+
+// To remove data using these defaults:
+app.use(mongoSanitize());
+
+const apiLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message : "Vous avez atteint le nombre de requête maximum, merci de réessayer dans un instant."
+})
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/api/auth', userRoutes);
+app.use('/api/auth', apiLimiter, userRoutes);
 app.use('/api/sauces', sauceRoutes);
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,24 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
+const passwordValidator = require('password-validator');
 const User = require('../models/User');
 
+
+// Create a schema for the passwords
+var schemaPassword = new passwordValidator();
+
+// Add properties to it
+schemaPassword
+.is().min(8)                    // Minimum length 8
+.is().max(100)                  // Maximum length 100
+.has().uppercase()              // Must have uppercase letters
+.has().lowercase()              // Must have lowercase letters
+.has().digits(1)                // Must have at least 1 digit
+
+
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
+  if (schemaPassword.validate(req.body.password)) {
+    bcrypt.hash(req.body.password, 10) // The password is encrypted with 10 salt rounds
       .then(hash => {
         const user = new User({
           email: req.body.email,
@@ -15,24 +29,27 @@ exports.signup = (req, res, next) => {
           .catch(error => res.status(400).json({ error }));
       })
       .catch(error => res.status(500).json({ error }));
+  } else {
+    return res.status(401).json({message : `Le mot de passe doit contenir au moins 8 caractères et être composé d'au moins 1 majuscule, 1 minuscule et 1 chiffre.`});
+  }
 };
 
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
       .then(user => {
         if (!user) {
-          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+          return res.status(401).json({ error : `Nous n'avons pas trouvé de compte correspondant à l'adresse e-mail renseignée.` });
         }
         bcrypt.compare(req.body.password, user.password)
           .then(valid => {
             if (!valid) {
-              return res.status(401).json({ error: 'Mot de passe incorrect !' });
+              return res.status(401).json({ error : `Le mot de passe renseigné est incorrect.` });
             }
             res.status(200).json({
               userId: user._id,
               token: jwt.sign(
                 { userId: user._id },
-                'RANDOM_TOKEN_SECRET',
+                process.env.TOKEN,
                 { expiresIn: '24h' }
               )
             });
